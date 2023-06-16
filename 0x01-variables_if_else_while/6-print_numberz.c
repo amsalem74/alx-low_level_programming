9d5e902f8ee822b5cda45d8845c43c2527a8f3d6
amsalem74 <abdelrhmansalem74@gmail.com>
2023-06-16 16:47:27 -0700

    numnerz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,13 +9,10 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n >= 9; n++)
+	for (n = 0; n < 10; n++)
 	{
-		
 		putchar(n + 48);
 	}
-
-		putchar('\n');
-		
-		return(0);
+	putchar('\n');
+	return(0);
 }
