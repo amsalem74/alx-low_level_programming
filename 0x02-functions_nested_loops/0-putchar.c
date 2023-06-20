2f3c35664837e3c8f4b584f132cbccf497519cab
amsalem74 <abdelrhmansalem74@gmail.com>
2023-06-20 10:48:44 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,17 @@
 #include "main.h"
 #include <unistd.h>
+#include <stdio.h>
+
 /**
-  * main - Entery point
-  * Discreption: 'print _putchar to stdout
-  * Return: always 0
-  */
-int main(void) {
+ * main - Entry point
+ *
+ * Description: Prints _putchar to stdout
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
