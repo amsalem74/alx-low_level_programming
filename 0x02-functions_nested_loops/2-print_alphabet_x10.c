7c1b6dae17413e77bddca4fe079428b4ce84a0a3
amsalem74 <abdelrhmansalem74@gmail.com>
2023-06-20 12:27:39 -0700

    alphabet10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - print _putchar
+ * 
+ * Return:Alawys 0
+ */
+void print_alphabet_x10(void)
+{
+	int a;
+	int b;
+
+	for (b = 1; a <= 122; b++)
+	{
+		_putchar(b);
+	}
+	_putchar('\n');
+}
