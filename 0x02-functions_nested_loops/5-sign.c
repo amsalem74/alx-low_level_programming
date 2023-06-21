29abd8096e5d36a973c9abaa1a459a05b51c5e3b
amsalem74 <abdelrhmansalem74@gmail.com>
2023-06-20 18:31:56 -0700

    last

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,22 +10,17 @@
  */
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else 
-	{
-		_putchar('-');
-		return (-1);
-	}
-	
-	_putchar('\n');
+	putchar('+');
+	return (1);
 }
-
+else if (n == 0)
+{
+	putchar('0');
+	return (0);
+}
+else
+{
+	putchar('-');
+	return (-1);
+	}
+}
