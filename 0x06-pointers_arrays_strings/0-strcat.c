dc05e48d1da0d9ef6d3b2133abc576cdb3f5db78
amsalem74 <abdelrhmansalem74@gmail.com>
2023-06-29 13:04:13 -0700

    new1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
-  * _stract - concatenates tow strings
+  * _strcat - concatenates two strings
   * @dest: input value
   * @src: input value
+  *
   * Return: void
   */
 char *_strcat(char *dest, char *src)
