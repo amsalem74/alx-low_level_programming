91f01e9b830e4fd5b3cdc148c29092ab6f59fde0
amsalem74 <abdelrhmansalem74@gmail.com>
2023-07-03 10:52:33 -0700

    llo world

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,16 +11,16 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; s[i]; i++)
-    {
-        for (j = 0; accept[j]; j++)
-        {
-            if (s[i] == accept[j])
-                return (s + i);
-        }
-    }
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+		}
+	}
 
-    return (NULL);
+	return (NULL);
 }
