b19ed25477611d77ecb165c01db003826af84ec4
amsalem74 <abdelrhmansalem74@gmail.com>
2023-07-03 10:38:21 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,23 +10,23 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int i, j;
-    unsigned int count = 0;
+	unsigned int i, j;
+	unsigned int count = 0;
 
-    for (i = 0; s[i]; i++)
-    {
-        for (j = 0; accept[j]; j++)
-        {
-            if (s[i] == accept[j])
-            {
-                count++;
-                break;
-            }
-        }
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				count++;
+				break;
+			}
+		}
 
-        if (!accept[j])
-            break;
-    }
+		if (!accept[j])
+			break;
+	}
 
-    return (count);
+	return (count);
 }
