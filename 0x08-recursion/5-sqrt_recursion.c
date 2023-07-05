e56ff130296d346fdd996779d9b1e47976dddd1b
amsalem74 <abdelrhmansalem74@gmail.com>
2023-07-05 13:14:01 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,24 +10,24 @@
  */
 int sqrt_helper(int n, int start, int end)
 {
-    int mid;
+	int mid;
 
-    if (start > end)
-        return -1;
+	if (start > end)
+		return (-1);
 
-    mid = (start + end) / 2;
+	mid = (start + end) / 2;
 
-    if (mid * mid == n)
-        return mid;
+	if (mid * mid == n)
+		return (mid);
 
-    if (mid * mid < n)
-    {
-        if ((mid + 1) * (mid + 1) > n)
-            return -1;
-        return sqrt_helper(n, mid + 1, end);
-    }
+	if (mid * mid < n)
+	{
+		if ((mid + 1) * (mid + 1) > n)
+			return (-1);
+		return (sqrt_helper(n, mid + 1, end));
+	}
 
-    return sqrt_helper(n, start, mid - 1);
+	return (sqrt_helper(n, start, mid - 1));
 }
 
 /**
@@ -38,8 +38,8 @@ int sqrt_helper(int n, int start, int end)
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return -1;
+	if (n < 0)
+		return (-1);
 
-    return sqrt_helper(n, 0, n);
+	return (sqrt_helper(n, 0, n));
 }
