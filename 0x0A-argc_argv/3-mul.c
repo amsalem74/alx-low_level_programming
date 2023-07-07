f1494f195628f898b5c52c645666df3df25fed5c
amsalem74 <abdelrhmansalem74@gmail.com>
2023-07-07 10:24:03 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,43 +1,28 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * _atoi - converts a string to a intger
- * @s: string to be coverted
+ * main - Multiply two numbers
+ * @argc: Number of arguments
+ * @argv: Array of arguments
  *
- * Return: the int converted from the string
+ * Return: 0 on success, 1 on error
  */
-int _atoi(char *s)
+int main(int argc, char *argv[])
 {
-	int i, d, n, len, f, digit;
+    int num1, num2, result;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-	while (s[len] != '\0')
-		len++;
-	while (i < len && f == 0)
-	{
-		if (s[i] == '_')
-			++d;
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
+    if (argc != 3)
+    {
+        printf("Error\n");
+        return 1;
+    }
 
-	if (f == 0)
-		return (0);
-	return (n);
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[2]);
+    result = num1 * num2;
+
+    printf("%d\n", result);
+
+    return 0;
 }
