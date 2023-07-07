25f02022cf54ef0d6a3511cc96d288e56f84b468
amsalem74 <abdelrhmansalem74@gmail.com>
2023-07-07 12:54:11 -0700

    library

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+int _atoi(char *s)
+{
+    int sign = 1;
+    int result = 0;
+    int i = 0;
+
+    if (s[0] == '-')
+    {
+        sign = -1;
+        i++;
+    }
+
+    for (; s[i] != '\0'; i++)
+    {
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            result = result * 10 + (s[i] - '0');
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return result * sign;
+}
