cdd6dfefa8e2635586e277137007a19f7cf599c3
amsalem74 <abdelrhmansalem74@gmail.com>
2023-07-13 14:53:07 -0700

    nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,7 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include "main.h"
+
 /**
  * string_nconcat - Concatenates two strings
  * @s1: First string
@@ -16,44 +16,37 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len1 = 0, len2 = 0, i, j;
 	char *concat;
 
+	/* Handle NULL strings */
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
+	/* Calculate lengths of the strings */
 	while (s1[len1] != '\0')
 		len1++;
 	while (s2[len2] != '\0')
 		len2++;
 
+	/* Adjust n if it's greater than or equal to s2 length */
 	if (n >= len2)
 		n = len2;
 
+	/* Allocate memory for the concatenated string */
 	concat = malloc(sizeof(char) * (len1 + n + 1));
 	if (concat == NULL)
 		return (NULL);
 
+	/* Copy s1 into concat */
 	for (i = 0; i < len1; i++)
 		concat[i] = s1[i];
+
+	/* Concatenate n bytes of s2 into concat */
 	for (j = 0; j < n; j++)
 		concat[i + j] = s2[j];
 
+	/* Add null-terminating character */
 	concat[i + j] = '\0';
 
 	return (concat);
-}
-/**
- * main - Entry point of the program
- *
- * Return: Always 0
- */
-int main(void)
-{
-	char *concat;
-
-	concat = string_nconcat("Best ", "School !!!", 6);
-	printf("%s\n", concat);
-	free(concat);
-
-	return (0);
 }
